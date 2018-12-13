bcc46cb21e012192bd25aedea157710c88aa3919
장 상련 <srjang@taihoinst.com>
2018-12-13 22:00:54 +0900

    수정

--- webapps/ROOT/assets/js/botchat.js
+++ webapps/ROOT/assets/js/botchat.js
@@ -20923,7 +20923,7 @@ function ssoConnection() {
     $.ajax({
         //type: "POST",
         type: "GET",
-        url: directLineUrl + "/api/conversations/" + $('#conversationId').val() + "/activities",
+        url: directLineUrl + "/api/conversations/" + $('#conversationId').val() + "/messages",
         dataType : "json",
         data: info,
         headers: {
