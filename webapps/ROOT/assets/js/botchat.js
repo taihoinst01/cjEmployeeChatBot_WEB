ca7501a0d162a65187da2e01024635ba7baec1d7
Sinook Kang <sokang337@gmail.com>
2018-12-13 17:38:48 +0900

    update

--- webapps/ROOT/assets/js/botchat.js
+++ webapps/ROOT/assets/js/botchat.js
@@ -3463,10 +3463,10 @@ var startFlag = 0;
                         //conversationId 추가
                         $('#conversationId').val(e.conversationId);
                         //KSO ssoConnection
-                        if (typeof (e.conversationId) == 'string' && startFlag == 0
-                            || location.href == 'https://cjemployeechatbot-web.azurewebsites.net/'
+                        if (typeof (e.conversationId) == 'string' && startFlag == 0 &&
+                            (location.href == 'https://cjemployeechatbot-web.azurewebsites.net/'
                             || location.href == 'https://cjemployeechatbot-web.azurewebsites.net/default.asp'
-                            || location.href == 'https://cjemployeechatbot-web.azurewebsites.net/default_m.asp') {
+                            || location.href == 'https://cjemployeechatbot-web.azurewebsites.net/default_m.asp')) {
                             console.log("SSO Connect");
                             ssoConnection();
                             startFlag = 1;
