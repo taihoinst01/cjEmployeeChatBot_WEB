9b8495a34940728f1bb97f29bb031f3a0b0778bd
Sinook Kang <sokang337@gmail.com>
2018-12-14 13:50:14 +0900

    update

--- webapps/ROOT/assets/js/botchat.js
+++ webapps/ROOT/assets/js/botchat.js
@@ -3471,7 +3471,7 @@ var defaultUrl_m = 'https://cjemployeechatbot-web.azurewebsites.net/default_m.as
                         if (typeof (e.conversationId) == 'string' && startFlag == 0 &&
                             (location.href == defaultUrl
                                 || location.href == defaultUrl_pc
-                                || location.href == defaultUrl_m)) {
+                                || location.href == defaultUrl_m + "?cjworld_id="+$('#cjworld_id').val)) {
                             console.log("SSO Connect");
                             ssoConnection();
                             startFlag = 1;
