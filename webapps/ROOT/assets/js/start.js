ba575bb4aac99f8e787c7bf6810ad50cd0bcc531
Sinook Kang <sokang337@gmail.com>
2018-12-14 14:30:35 +0900

    update

--- webapps/ROOT/assets/js/start.js
+++ webapps/ROOT/assets/js/start.js
@@ -53,13 +53,13 @@ if (isMobile()) {   //모바일 환경
 	//	location.href = "index_m.html";
     //});
 
-    console.log('value = '+ $('#cjworld_id').val());
+    console.log('value = '+ $('#cjworld_id_get').val());
 
     //2. 로딩시 표출
     
 
     if ($('#cjworld_id').val() != '') {
-        location.href = "default_m.asp?cjworld_id="+$('#cjworld_id').val();
+        location.href = "default_m.asp?cjworld_id="+$('#cjworld_id_get').val();
     } else {
         location.href = "default_m.asp";
     }
