3ce89b44b0e9a9fd79bb152d9b77c5ffc20def11
Sinook Kang <sokang337@gmail.com>
2018-12-26 11:49:16 +0900

    update

--- webapps/ROOT/assets/js/mobile/m_ui.js
+++ webapps/ROOT/assets/js/mobile/m_ui.js
@@ -201,7 +201,12 @@ $(function () {
             $('.menuBox').removeClass('off').addClass('on').css({ 'display': 'block' });
             $('.menuIcon').removeClass('menuIcon').addClass('menuIcon_active');
             $('.sendIcon').removeClass('sendIcon').addClass('sapIcon');
-            $('.wc-message-groups').css({ 'height': initGroupHeight + 'px', 'bottom': 165 + 'px' }).scrollTop($('.wc-message-group-content')[0].scrollHeight);
+
+            if (initGroupHeight == $('.wc-message-groups').height()) {
+                $('.wc-message-groups').css({ 'height': $('.wc-message-groups').height() - 96 + 'px', 'bottom': 165 + 'px' }).scrollTop($('.wc-message-group-content')[0].scrollHeight);
+            } else {
+                $('.wc-message-groups').css({ 'height': initGroupHeight + 'px', 'bottom': 165 + 'px' }).scrollTop($('.wc-message-group-content')[0].scrollHeight);
+            }
             $('.wc-console').css({ 'bottom': 115 + 'px' });
         } else if ($(this).hasClass('menuIcon_active')) {   //MENU 닫기
             $('.menuBox').removeClass('on').addClass('off').css({ 'display': 'none' });
