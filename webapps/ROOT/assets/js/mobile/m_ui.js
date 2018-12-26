575c737d3f975494ead277dad0e03300f2ee353a
Sinook Kang <sokang337@gmail.com>
2018-12-26 16:01:13 +0900

    update

--- webapps/ROOT/assets/js/mobile/m_ui.js
+++ webapps/ROOT/assets/js/mobile/m_ui.js
@@ -196,6 +196,7 @@ $(function () {
     });
     //챗봇 MENU + SAP 버튼 동작
     $('.wc-menu > div').click(function () {
+        $('.wc-message-groups').trigger('click');
         $('.wc-shellinput').attr('value', '').attr('placeholder', '궁금한 것을 물어보세요!').val('');
 
         if ($(this).hasClass('menuIcon')) {     //MENU 열기
@@ -210,7 +211,7 @@ $(function () {
             //$('.wc-message-groups').scrollTop($('.wc-message-group-content')[0].scrollHeight);
 
             //$('.wc-message-groups').css({ 'height': $('.wc-message-groups').height() - 90 + 'px' });
-            $('.wc-message-groups').css({ 'height': initGroupHeight - 105 + 'px' });
+            $('.wc-message-groups').css({ 'height': initGroupHeight - 116 + 'px' });
             $('.wc-console').css({ 'bottom': 115 + 'px' });
             $('.menuBox').removeClass('off').addClass('on').css({ 'display': 'block' });
             $('.menuIcon').removeClass('menuIcon').addClass('menuIcon_active');
