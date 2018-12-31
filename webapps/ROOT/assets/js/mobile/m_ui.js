26211f47a7488956459c67fa01286a83c7c1da3a
Sinook Kang <sokang337@gmail.com>
2018-12-31 18:06:53 +0900

    update

--- webapps/ROOT/assets/js/mobile/m_ui.js
+++ webapps/ROOT/assets/js/mobile/m_ui.js
@@ -290,8 +290,6 @@ $(function () {
         }
         $('.menuBox').removeClass('on').addClass('off').css({ 'display': 'none' });
         $('.wc-message-groups').removeClass('menuboxOn').addClass('menuboxOff').scrollTop($('.wc-message-group-content')[0].scrollHeight);
-
-        $('.wc-send > div').trigger('click');
     });
     $('.wc-send > div').click(function () {
         var v = $('input[type="text"].wc-shellinput').val();
